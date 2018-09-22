eed378976391fdd3b1ca2be79512ff34c5c374f5
jko6888 <43213100+jko6888@users.noreply.github.com>
2018-09-22 02:19:27 -0400

    Update JKoLib.java

--- JKoLib.java
+++ JKoLib.java
@@ -2,13 +2,13 @@ package JamesKo;
 
 public class JKoLib {
     public static boolean isPalindrome(String str) {
-        String word = "";
+        String word = ""; //sets up a empty string
         int i = str.length() - 1;
         while (i >= 0) {
             word = word + str.substring(i, i + 1);
             i--;
         }
-        if (word.equals(str)) {
+        if (word.equals(str)) { //test to see if
             return true;
         } else {
             return false;
@@ -16,9 +16,40 @@ public class JKoLib {
     }
 
     public static String dateStr(String date) {
-        String a = date.substring(3, 5);
-        String b = date.substring(0, 2);
-        String c = date.substring(6, 10);
-        return (a + "-" + b + "-" + c);
+        String x = date.substring(3, 5);
+        String y = date.substring(0, 2);
+        String z = date.substring(6, 10);    //removes the day, month, and year and adds them to a new string
+        return (x + "-" + y + "-" + z);
     }
 
+
+    public static int sumUpTo(int x) {
+        int b = 0;
+        for (int a = 0; a < x; a++) {
+            b = b + a; //adds the previous numbers with the sum up to
+        }
+        return (b);
+    }
+
+    public static String multiplicationTable(int start, int length) {
+        String multiply = "";
+        for (int i = 0; i <= length; i++) {
+            int num = start * i;
+            multiply = multiply + start + "*" + i + "=" + num + "__"; //multiples the base with the numbers up to the range
+        }
+        return multiply;
+    }
+
+    public static boolean isFibonacci(int x) {
+        int num1 = 1;
+        int check2 = 0;
+        for (int check3 = 0; num1 < x; num1 = check3) {
+            check3 = num1 + check2;
+            check2 = num1;
+            if (check3 == x)
+                return true;
+
+        }
+        return false;
+    }
+}
